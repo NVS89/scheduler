3ab2801c5633607a379ad940885cd7ee7f066e9a
mykyta.shchepetkov <mykyta.shchepetkov@synevo.ua>
2020-01-31 14:04:16 +0200

    fixed time delta calculation;

--- src/app/calendar/current-time-marker/current-time-marker.component.ts
+++ src/app/calendar/current-time-marker/current-time-marker.component.ts
@@ -12,6 +12,7 @@ export class CurrentTimeMarkerComponent implements OnInit, OnDestroy {
     redrawTimeMarkerSubscription: Subscription;
     position = 0;
     fiveMinutes = 50000;
+    oneHour = 60;
 
     constructor() { }
 
@@ -27,8 +28,15 @@ export class CurrentTimeMarkerComponent implements OnInit, OnDestroy {
 
     setPositionOfMarker() {
         const currentMinute = new Date().getMinutes();
-        const timeDelta = currentMinute > this.segmentStep ? (currentMinute - this.segmentStep) : currentMinute;
+        const timeDelta = currentMinute > this.segmentStep ? (this.oneHour - currentMinute - this.segmentStep) : currentMinute;
         this.position = (timeDelta / this.segmentStep) * this.segment.elemHeigth;
+        console.log('===================================================');
+        console.log(currentMinute);
+        console.log(this.segmentStep);
+        console.log(timeDelta);
+        console.log();
+        console.log();
+        console.log('===================================================');
     }
 
     ngOnDestroy() {
