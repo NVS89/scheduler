f607b8e5248076903180b44ad789c0317d908cfa
mykyta.shchepetkov <mykyta.shchepetkov@synevo.ua>
2020-01-31 15:16:40 +0200

    edited styles; added placeholders;

--- src/app/calendar/current-time-marker/current-time-marker.component.ts
+++ src/app/calendar/current-time-marker/current-time-marker.component.ts
@@ -10,12 +10,17 @@ export class CurrentTimeMarkerComponent implements OnInit, OnDestroy {
     @Input() segment;
     @Input() segmentStep: number;
     redrawTimeMarkerSubscription: Subscription;
+    currentTime: { hour: string, minutes: string };
     position = 0;
     fiveMinutes = 50000;
 
     constructor() { }
 
     ngOnInit() {
+        this.currentTime = {
+            hour: this.formatTime(new Date().getHours()),
+            minutes: this.formatTime(new Date().getMinutes())
+        };
         const redrawTimeMarker = interval(this.fiveMinutes);
         this.setPositionOfMarker();
         this.redrawTimeMarkerSubscription = redrawTimeMarker.subscribe(
@@ -25,11 +30,19 @@ export class CurrentTimeMarkerComponent implements OnInit, OnDestroy {
         );
     }
 
+    formatTime(time) {
+        return time < 10 ? '0' + time : time.toString();
+    }
+
     setPositionOfMarker() {
         const currentMinute = new Date().getMinutes();
         const timeDelta = (currentMinute / this.segmentStep);
         const proportion = currentMinute > this.segmentStep ? timeDelta - Math.trunc(timeDelta) : timeDelta;
         this.position = proportion * this.segment.elemHeigth;
+        this.currentTime = {
+            hour: this.formatTime(new Date().getHours()),
+            minutes: this.formatTime(new Date().getMinutes())
+        };
     }
 
     ngOnDestroy() {
